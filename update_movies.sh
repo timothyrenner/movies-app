ce2406fbe26e3d0550b95b0dab38be7ac3363dbb
Timothy Renner <renner.timothy@gmail.com>
2022-07-18 20:13:05 -0500

    Update the shell script so it runs properly within the vault.

--- update_movies.sh
+++ update_movies.sh
@@ -1,5 +1,5 @@
-go run main.go update-movies
-conda run movies-app dvc add movie_vault data/movies.db
-git add movie_vault.dvc data/movie.db.dvc
+go run ../main.go update-movies ../movie_vault
+conda run movies-app dvc add ../movie_vault ../data/movies.db
+git add ../movie_vault.dvc ../data/movie.db.dvc
 git commit -m "Update movies $(date)."
 git push

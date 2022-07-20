0be1b72bff85160c3abf2726d275bec8d4f26927
Timothy Renner <renner.timothy@gmail.com>
2022-07-19 20:54:48 -0500

    Fixed the watch dates ... again.

---
+++ migrations/20220720010704_fix-watch-dates-again.up.sql
@@ -0,0 +1,9 @@
+UPDATE movie_watch
+SET watched = DATE(watched, '+1 day')
+WHERE (
+    (watched >= '2020-11-02') AND
+    (watched <= '2021-03-13')
+) OR (
+    (watched >= '2021-11-08') AND
+    (watched <= '2022-03-12')
+);

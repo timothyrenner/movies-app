92769d67678b43172d2e48277414df008d4175cd
Timothy Renner <renner.timothy@gmail.com>
2022-07-25 18:42:13 -0500

    Fixed up script so it runs in obsidian.

--- update_movies.sh
+++ update_movies.sh
@@ -1,5 +1,6 @@
-go run ../main.go update-movies ../movie_vault
-conda run movies-app dvc add ../movie_vault ../data/movies.db
-git add ../movie_vault.dvc ../data/movie.db.dvc
+go run main.go update-movies movie_vault
+conda run -n movies-app dvc add movie_vault data/movies.db
+git add movie_vault.dvc data/movies.db.dvc
 git commit -m "Update movies $(date)."
 git push
+conda run -n movies-app dvc push

3e7c7e263d88d10b7bd02773873661e426bf53be
Timothy Renner <renner.timothy@gmail.com>
2022-07-25 18:44:25 -0500

    Use --no-capture-output with DVC commands in update movies shell script.

--- update_movies.sh
+++ update_movies.sh
@@ -1,6 +1,6 @@
 go run main.go update-movies movie_vault
-conda run -n movies-app dvc add movie_vault data/movies.db
+conda run --no-capture-output -n movies-app dvc add movie_vault data/movies.db
 git add movie_vault.dvc data/movies.db.dvc
 git commit -m "Update movies $(date)."
 git push
-conda run -n movies-app dvc push
+conda run --no-capture-output -n movies-app dvc push
